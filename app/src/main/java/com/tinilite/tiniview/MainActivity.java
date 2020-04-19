783e79eee4c04b45a3ad826770963e1c0ce9fdae
J.P <jpdenoyer@gmail.com>
2020-04-19 16:36:10 -0400

    041920 JPCF deleted unused mainactivity/databinding declaration

--- app/src/main/java/com/tinilite/tiniview/MainActivity.java
+++ app/src/main/java/com/tinilite/tiniview/MainActivity.java
@@ -1,7 +1,7 @@
 package com.tinilite.tiniview;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.AsyncTask;
+
 import android.os.Bundle;
 import android.util.Log;
 
@@ -9,15 +9,12 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
     /*controls*/
-    //private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.d(TAG, "onCreate: beginning.......");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
         Log.d(TAG, "onCreate: finished");
     }
 
