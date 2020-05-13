121ec37477eb851476a8854941983aa1aba71ea5
J.P <jpdenoyer@gmail.com>
2020-05-12 22:19:55 -0400

    051220 10:13P JP replace fragments in single frame layout

--- app/src/main/java/com/tinilite/tiniview/LoginFragment.java
+++ app/src/main/java/com/tinilite/tiniview/LoginFragment.java
@@ -100,7 +100,7 @@ public class LoginFragment extends Fragment {
                         true
                 );
 
-                mListener.OnLogingDisplayWelcomeFragment();
+                //mListener.OnLogingDisplayWelcomeFragment();
             }
           }
 
--- app/src/main/java/com/tinilite/tiniview/MainActivity.java
+++ app/src/main/java/com/tinilite/tiniview/MainActivity.java
@@ -1,5 +1,6 @@
 package com.tinilite.tiniview;
 
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
@@ -28,140 +29,60 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null)
-           displayWelcomeFragment();
+        // Check that the activity is using the layout version with
+        // the fragment_container FrameLayout
+        if (findViewById(R.id.fragment_container) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, WelcomeFragment.newInstance()).commit();
+        }
         Log.d(TAG, "onCreate: finished");
     }
 
-    @Override
-    protected void onStart()
-    {
-        super.onStart();
-/*
-        if (!loaded) {
-            displayWelcomeFragment();
-            loaded = true;
-        }*/
-        //Toast.makeText(getApplicationContext(),"Now onStart() calls", Toast.LENGTH_LONG).show(); //onStart Called
-    }
+    public void displayFragment(Fragment fragment) {
+        Log.d(TAG, "displayFragment: beginning.......");
 
+        //get FragmentManager
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        //start a transaction
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        // Replace whatever is in the fragment_container view with this fragment,
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        // add the transaction to the back stack so the user can navigate back
+        fragmentTransaction.addToBackStack(null);
+        // Commit the transaction
+        fragmentTransaction.commit();
+
+        Log.d(TAG, "displayFragment: finished");
+    }
 
 
     public void displayDisplayFragment() {
         Log.d(TAG, "displayDisplayFragment: beginning.......");
-
-        closeLoginFragment();
-        closeWelcomeFragment();
-
-        DisplayFragment displayFragmentFragment = DisplayFragment.newInstance();
-
-        //get FragmentManager and start a transaction
-        //FragmentManager fragmentManager =  getSupportFragmentManager();
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-
-        //Add the simple fragment
-        fragmentTransaction.add(R.id.display_fragment_container,
-                displayFragmentFragment).addToBackStack(null).commit();
-
+        displayFragment(DisplayFragment.newInstance());
         Log.d(TAG, "displayDisplayFragment: finished");
     }
 
-    public void closeDisplayFragment(){
-        Log.d(TAG, "closeWelcomeFragment: beginning........");
-        //get the fragment manager
-        //FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentManager fragmentManager = getSupportFragmentManager() ;
-        //check to see if the fragment is alredy showing
-        DisplayFragment displayFragment = (DisplayFragment) fragmentManager
-                .findFragmentById(R.id.display_fragment_container);
-        if(displayFragment != null){
-            //create and commit the transaction to remove the fragment
-            FragmentTransaction fragmentTransaction =
-                    fragmentManager.beginTransaction();
-            fragmentTransaction.remove(displayFragment).commit();
-        }
-
-        Log.d(TAG, "closeWelcomeFragment: finished");
-    }
-
-
     public void displayWelcomeFragment() {
         Log.d(TAG, "displayWelcomeFragment: beginning.......");
-
-        closeLoginFragment();
-        closeDisplayFragment();
-
-        WelcomeFragment welcomeFragment = WelcomeFragment.newInstance();
-
-        //get FragmentManager and start a transaction
-        //FragmentManager fragmentManager =  getSupportFragmentManager();
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-
-        //Add the simple fragment
-        fragmentTransaction.add(R.id.Welcome_fragment_container,
-                welcomeFragment).addToBackStack(null).commit();
-
+        displayFragment(WelcomeFragment.newInstance());
         Log.d(TAG, "displayWelcomeFragment: finished");
     }
-
-    public void closeWelcomeFragment(){
-        Log.d(TAG, "closeWelcomeFragment: beginning........");
-        //get the fragment manager
-        //FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentManager fragmentManager = getSupportFragmentManager() ;
-        //check to see if the fragment is alredy showing
-        WelcomeFragment welcomeFragment = (WelcomeFragment) fragmentManager
-                .findFragmentById(R.id.Welcome_fragment_container);
-        if(welcomeFragment != null){
-            //create and commit the transaction to remove the fragment
-            FragmentTransaction fragmentTransaction =
-                    fragmentManager.beginTransaction();
-            fragmentTransaction.remove(welcomeFragment).commit();
-        }
-
-        Log.d(TAG, "closeWelcomeFragment: finished");
-    }
-
 
 
     public  void displayLoginFragment() {
         Log.d(TAG, "displayLoginFragment: beginning.......");
-
-        closeWelcomeFragment();
-        closeDisplayFragment();
-
-        LoginFragment loginFragment = LoginFragment.newInstance();
-
-        //get FragmentManager and start a transaction
-        //FragmentManager fragmentManager =  getSupportFragmentManager();
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-
-        //Add the simple fragment
-        fragmentTransaction.add(R.id.login_fragment_container,
-                loginFragment).addToBackStack(null).commit();
-
+        displayFragment(LoginFragment.newInstance());
         Log.d(TAG, "displayLoginFragment: finished");
-    }
-
-    public void closeLoginFragment(){
-        Log.d(TAG, "closeWelcomeFragment: beginning........");
-        //get the fragment manager
-        //FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentManager fragmentManager = getSupportFragmentManager() ;
-        //check to see if the fragment is alredy showing
-        LoginFragment loginFragment = (LoginFragment) fragmentManager
-                .findFragmentById(R.id.login_fragment_container);
-        if(loginFragment != null){
-            //create and commit the transaction to remove the fragment
-            FragmentTransaction fragmentTransaction =
-                    fragmentManager.beginTransaction();
-            fragmentTransaction.remove(loginFragment).commit();
-        }
-
-        Log.d(TAG, "closeWelcomeFragment: finished");
     }
 
 
@@ -193,7 +114,6 @@ public class MainActivity extends AppCompatActivity
 
         return credentials;
     }
-
 
     @Override
     public void OnWelcomeLoggon() {
