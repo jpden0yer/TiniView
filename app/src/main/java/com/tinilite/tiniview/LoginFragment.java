c6536495961c9f89219881e723bc2cf458b46489
J.P <jpdenoyer@gmail.com>
2020-05-17 15:50:32 -0400

    051720 JPCF 0345P reformat gradle partially fail

--- app/src/main/java/com/tinilite/tiniview/LoginFragment.java
+++ app/src/main/java/com/tinilite/tiniview/LoginFragment.java
@@ -1,7 +1,9 @@
 package com.tinilite.tiniview;
 
+//import android.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
+
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
